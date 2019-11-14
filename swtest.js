e4e32b45e00e7520296a739b462adaa65cf271ad
Kornel Lesiński <kornel@cloudflare.com>
2019-11-14 21:11:02 +0000

    Test

--- swtest.js
+++ swtest.js
@@ -1,3 +1,11 @@
 self.addEventListener('fetch', event => {
-    return event.respondWith(fetch(event.request.url + '?' + Math.random()));
+    return event.respondWith(handleEvent(event));
 });
+
+async function handleEvent(event) {
+    const res = await fetch(event.request.url + '?' + Math.random());
+    return new Response(res.body, {
+        status: res.status,
+        headers: res.headers,
+    });
+}
