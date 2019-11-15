616477b536b7fd4b5ba054a7e27a8985eeeb1f4e
Kornel Lesiński <kornel@cloudflare.com>
2019-11-15 11:53:06 +0000

    return await

--- swtest.js
+++ swtest.js
@@ -1,8 +1,6 @@
 
 self.addEventListener('fetch', event => {
-    const url = event.request.url;
     return event.respondWith(async function(){
-        const res = await fetch(event.request);
-        return res;
+        return await fetch(event.request);
     }());
 });
