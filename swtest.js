920d398e0bd228f86d667dc1f86a52fb3105786a
Kornel Lesiński <kornel@cloudflare.com>
2019-11-15 10:42:39 +0000

    Argh

--- swtest.js
+++ swtest.js
@@ -1,12 +1,30 @@
-self.addEventListener('fetch', event => {
-    return event.respondWith(handleEvent(event));
-});
 
-async function handleEvent(event) {
-    const res = await fetch(event.request.url + '?' + Math.random());
-    const [body1, body2] = res.body.tee();
-    return new Response(body1, {
-        status: res.status,
-        headers: res.headers,
-    });
-}
+self.addEventListener('fetch', event => {
+    const url = event.request.url;
+    return event.respondWith(async function(){
+        if (!/\.jpg/.test(url)) {
+            return fetch(event.request);
+        }
+        const clientId = event.clientId;
+        const res = await fetch(event.request);
+        if (!res.body) { // cross-origin?
+            return res;
+        }
+        // Split the stream, so that the page can get it unmodified without having to wait for
+        // the ServiceWorker processing the chunks.
+        const [body1, body2] = res.body.tee();
+        const reader = body2.getReader();
+        let readSoFar = 0;
+        let prevTime = Date.now();
+
+
+
+        return new Response(body1, {
+            status: res.status,
+            headers: res.headers,
+        });
+    }().catch(e => {
+        console.error("sw", url, e);
+        throw e;
+    }));
+});
