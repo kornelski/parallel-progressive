cb5d376ee818f1056208a351dcbd8d510602e504
Kornel Lesiński <kornel@cloudflare.com>
2019-11-15 11:50:36 +0000

    Rly?

--- swtest.js
+++ swtest.js
@@ -4,12 +4,5 @@ self.addEventListener('fetch', event => {
     return event.respondWith(async function(){
         const res = await fetch(event.request);
         return res;
-        // return new Response(res.body, {
-        //     status: res.status,
-        //     headers: res.headers,
-        // });
-    }().catch(e => {
-        console.error("sw", url, e);
-        throw e;
-    }));
+    }());
 });
