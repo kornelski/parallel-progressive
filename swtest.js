d0a322cbb561b7fa833ca58ff6194da72693661d
Kornel Lesiński <kornel@cloudflare.com>
2019-11-15 11:45:30 +0000

    Simplify

--- swtest.js
+++ swtest.js
@@ -2,25 +2,12 @@
 self.addEventListener('fetch', event => {
     const url = event.request.url;
     return event.respondWith(async function(){
-        if (!/\.jpg/.test(url)) {
-            return fetch(event.request);
-        }
-        const clientId = event.clientId;
         const res = await fetch(event.request);
-        if (!res.body) { // cross-origin?
-            return res;
-        }
-        // Split the stream, so that the page can get it unmodified without having to wait for
-        // the ServiceWorker processing the chunks.
-        // const [body1, body2] = res.body.tee();
-        // const reader = body2.getReader();
-
-
-
-        return new Response(res.body, {
-            status: res.status,
-            headers: res.headers,
-        });
+        return res;
+        // return new Response(res.body, {
+        //     status: res.status,
+        //     headers: res.headers,
+        // });
     }().catch(e => {
         console.error("sw", url, e);
         throw e;
